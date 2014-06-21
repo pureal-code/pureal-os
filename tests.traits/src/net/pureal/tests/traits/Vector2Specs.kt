aa76b24501f844596ed29de9cb9706ab5196699e
Julius Kunze <juliuskunze@gmail.com>
2014-06-21 19:18:26 +0200

    finished test, still test engine failure

--- tests.traits/src/net/pureal/tests/traits/Vector2Specs.kt
+++ tests.traits/src/net/pureal/tests/traits/Vector2Specs.kt
@@ -4,11 +4,16 @@ import org.spek.Spek
 import net.pureal.traits.*
 
 class Vector2Specs : Spek() {{
-    given("some vectors") {
+    given("two unit vectors in x- and y-direction") {
         val eX = vectorOf(1,0)
         val eY = vectorOf(0,1)
 
-        on("") {it("") {
-        }}
+        on("getting the dot product of the two") {
+            val p = eX * eY
+
+            it("should be 0.") {
+                assert(zeroVector2 == p)
+            }
+        }
     }
 }}
