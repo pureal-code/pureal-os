1cf6331a57e7eaec43e865d665a2df639773f360
Stefan Noack <stefan@noack-software.de>
2014-07-04 00:23:53 +0200

    WIP OpenGL backend implementation

--- android.app/src/net/pureal/android/app/PurealActivity.kt
+++ android.app/src/net/pureal/android/app/PurealActivity.kt
@@ -7,27 +7,14 @@ import android.opengl.GLSurfaceView.Renderer
 import javax.microedition.khronos.opengles.GL10
 import javax.microedition.khronos.egl.EGLConfig
 import android.opengl.GLES20
+import net.pureal.android.backend.GlScreen
+import net.pureal.shell.Shell
 
 class PurealActivity : Activity() {
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(object : GLSurfaceView(this) {
-            {
-                setEGLContextClientVersion(2)
-                setEGLConfigChooser(8, 8, 8, 8, 16, 0)
-                setRenderer(object : Renderer {
-                    override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
-                        GLES20.glClearColor(0.0f, 0.0f, 1.0f, 1.0f)
-                    }
-                    override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
-                        GLES20.glViewport(0, 0, width, height)
-                    }
-                    override fun onDrawFrame(gl: GL10?) {
-                        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
-                    }
-                })
-                setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY)
-            }
-        })
+        val screen = GlScreen(this)
+        setContentView(screen);
+        Shell(screen);
     }
 }
