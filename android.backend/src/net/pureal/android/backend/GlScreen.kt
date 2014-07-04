becfd7b0a1e833ed21c2dac2d75910f13bdd929a
Stefan Noack <stefan@noack-software.de>
2014-07-04 15:22:49 +0200

    openGL draw rectangle

--- android.backend/src/net/pureal/android/backend/GlScreen.kt
+++ android.backend/src/net/pureal/android/backend/GlScreen.kt
@@ -12,15 +12,22 @@ import java.nio.ByteBuffer
 import java.nio.ByteOrder
 import java.nio.FloatBuffer
 import java.nio.ShortBuffer
+import net.pureal.traits.interaction.PointerInput
+import net.pureal.traits.math.Rectangle
+import net.pureal.traits.math.Shape
 
 class GlScreen(activity: Activity) : GLSurfaceView(activity), Screen {
     {
         setEGLContextClientVersion(2)
         setEGLConfigChooser(8, 8, 8, 8, 16, 0)
+    }
+    var program : Int? = null// = createProgram();
+    {
         setRenderer(object : Renderer {
             override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
                 GLES20.glClearColor(0.0f, 0.0f, 1.0f, 1.0f)
-                GLES20.glUseProgram(program);
+                program = createProgram();
+                GLES20.glUseProgram(program!!);
             }
             override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
                 GLES20.glViewport(0, 0, width, height)
@@ -32,16 +39,24 @@ class GlScreen(activity: Activity) : GLSurfaceView(activity), Screen {
         })
         setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY)
     }
-    val program = createProgram()
-    var content: GlComposedElement = GlComposedElement(composed(setOf()))
+    var content: GlComposedElement = GlComposedElement(composed(setOf()), this)
     override val size: Vector2 = vector(1, 1)
     override fun show(content: Composed) {
         when (content) {
             is GlComposedElement -> {
                 this.content = content
             }
-            else -> show(GlComposedElement(content))
+            else -> show(GlComposedElement(content, this))
         }
+    }
+    override fun elementAt(location: Vector2): Element {
+        throw UnsupportedOperationException()
+    }
+    override fun absoluteTransform(element: Element): Transform2 {
+        throw UnsupportedOperationException()
+    }
+    override fun relativePointerInput(element: Element): PointerInput {
+        throw UnsupportedOperationException()
     }
 }
 
@@ -81,7 +96,7 @@ class GlComposedElement(original: Composed, screen: GlScreen) : GlElement(origin
     override val removed: Trigger<GlElement> = trigger();
     {
         original.added += {(addedElement) ->
-            val glElement = glElement(addedElement)
+            val glElement = glElement(addedElement, screen)
             elements.add(glElement)
             added(glElement)
         }
@@ -93,8 +108,6 @@ class GlComposedElement(original: Composed, screen: GlScreen) : GlElement(origin
             }
         }
     }
-    override val coordinates = floatArray()
-    override val drawOrder = shortArray()
     override fun draw() = elements forEach { it.draw() }
 }
 
@@ -102,30 +115,46 @@ fun glElement(original: Element, screen: GlScreen): GlElement {
     return when (original) {
         is GlElement -> original
         is Composed -> GlComposedElement(original, screen)
-        is Rectangle -> GlRectangle(original)
+        is ShapeElement -> GlShapeElement(original, screen)
         else -> throw UnsupportedOperationException("No OpenGL implementation for element '${original}'.")
     }
 }
 
-abstract class GlElement(open val original: Element?, val screen : GlScreen) : Element {
+abstract class GlElement(open val original: Element?, val screen: GlScreen) : Element {
     override val transform: Transform2 get() = (original as? Element)?.transform ?: Transforms2.identity
     override val changed: Observable<Unit> get() = (original as? Element)?.changed ?: observable()
-    abstract val coordinates: FloatArray
-    abstract val drawOrder: ShortArray
+    abstract fun draw()
+}
+
+class GlShapeElement(override val original: ShapeElement, screen: GlScreen) : GlElement(original, screen), ShapeElement {
+    override val shape: GlShape = glShape(original.shape)
+    override val fill: Fill = object : InvisibleFill {}
+    override val stroke: Stroke = object : InvisibleStroke {}
+    override fun draw() {
+        val positionHandle = GLES20.glGetAttribLocation(screen.program!!, "vPosition")
+        GLES20.glEnableVertexAttribArray(positionHandle)
+        GLES20.glVertexAttribPointer(positionHandle, 3, GLES20.GL_FLOAT, false, 3 * 4, vertexBuffer)
+        val colorHandle = GLES20.glGetUniformLocation(screen.program!!, "vColor")
+        GLES20.glUniform4fv(colorHandle, 1, floatArray(1f, 0f, 0f, 1f), 0)
+        GLES20.glDrawElements(
+                GLES20.GL_TRIANGLE_FAN, shape.drawOrder.size,
+                GLES20.GL_UNSIGNED_SHORT, drawListBuffer)
+        GLES20.glDisableVertexAttribArray(positionHandle);
+    }
     private var vertexBuffer = buildVertexBuffer()
     private var drawListBuffer = buildDrawListBuffer()
     fun buildVertexBuffer(): FloatBuffer {
         // 4 bytes per float
-        val byteBuffer = ByteBuffer.allocateDirect(coordinates.size * 4) order ByteOrder.nativeOrder()!!;
+        val byteBuffer = ByteBuffer.allocateDirect(shape.coordinates.size * 4) order ByteOrder.nativeOrder()!!;
         val floatBuffer = byteBuffer.asFloatBuffer()
-        floatBuffer put coordinates position 0
+        floatBuffer put shape.coordinates position 0
         return floatBuffer
     }
     fun buildDrawListBuffer(): ShortBuffer {
         // 2 bytes per short
-        val byteBuffer = ByteBuffer.allocateDirect(coordinates.size * 2) order ByteOrder.nativeOrder()!!;
+        val byteBuffer = ByteBuffer.allocateDirect(shape.coordinates.size * 2) order ByteOrder.nativeOrder()!!;
         val shortBuffer = byteBuffer.asShortBuffer()
-        shortBuffer put drawOrder position 0
+        shortBuffer put shape.drawOrder position 0
         return shortBuffer
     }
     {
@@ -135,22 +164,18 @@ abstract class GlElement(open val original: Element?, val screen : GlScreen) : E
             // how? redraw()
         }
     }
-    open fun draw() {
-        val positionHandle = GLES20.glGetAttribLocation(screen.program, "vPosition")
-        GLES20.glEnableVertexAttribArray(positionHandle)
-        GLES20.glVertexAttribPointer(positionHandle, 3, GLES20.GL_FLOAT, false, 3*4, vertexBuffer)
-        val colorHandle = GLES20.glGetUniformLocation(screen.program, "vColor")
-        GLES20.glUniform4fv(colorHandle, 1, floatArray(1f, 0f, 0f, 1f), 0)
-        GLES20.glDrawElements(GLES20.GL_TRIANGLE_STRIP, 0, drawOrder.size, drawListBuffer)
-        GLES20.glDisableVertexAttribArray(positionHandle);
-    }
-
 }
 
-class GlRectangle(override val original: Rectangle, screen : GlScreen) : GlElement(original, screen), Rectangle {
+abstract class GlShape(open val original: Shape) : Shape {
+    override fun contains(location: Vector2): Boolean = original.contains(location)
+    abstract val coordinates: FloatArray
+    abstract val drawOrder: ShortArray
+}
+
+class GlRectangle(override val original: Rectangle) : GlShape(original) {
     override val coordinates: FloatArray get() {
-        val x = size.x.toFloat() / 2
-        val y = size.y.toFloat() / 2
+        val x = original.size.x.toFloat() / 2
+        val y = original.size.y.toFloat() / 2
         val z = 0f
         return floatArray(
                 +x, +y, z, // 0 top right
@@ -160,7 +185,12 @@ class GlRectangle(override val original: Rectangle, screen : GlScreen) : GlEleme
         )
     }
     override val drawOrder = shortArray(0, 1, 2, 3)
-    override val size: Vector2 get() = original.size
-    override val fill: Fill = null!!
-    override val stroke: Stroke = null!!
+}
+
+fun glShape(original : Shape) : GlShape {
+    return when (original) {
+        is GlShape -> original
+        is Rectangle -> GlRectangle(original)
+        else -> throw UnsupportedOperationException("No OpenGL implementation for shape '${original}'.")
+    }
 }
