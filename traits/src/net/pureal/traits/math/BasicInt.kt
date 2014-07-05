c4c7f9411e0d89f6223504fc049f93a05275f2c0
Maximilian Wende <maximilian.wende@googlemail.com>
2014-07-05 11:14:42 +0200

    using BigInteger now and writing constructors and math functions (WIP)

--- traits/src/net/pureal/traits/math/BasicInt.kt
+++ traits/src/net/pureal/traits/math/BasicInt.kt
@@ -1,6 +1,7 @@
 package net.pureal.traits.math
 
 import com.sun.javaws.exceptions.InvalidArgumentException
+import java.math.BigInteger
 
 
 /** Basic Int:
@@ -15,44 +16,23 @@ public trait BasicInt : BasicReal {
 
     override fun minus() : BasicInt = basicInt(number, !sign)
 
-
-
     override fun isInteger() : Boolean = true
 }
 
 // It must still be evaluated if the signed Int makes problems and if wae can use an unsigned Int or if we need w/e
-fun basicInt(i : Int) : BasicInt = basicInt(i.toLong())
+fun basicInt(i : Int) : BasicInt = basicInt(i.toString())
 
-fun basicInt(s : Short) : BasicInt = basicInt(s.toLong())
+fun basicInt(s : Short) : BasicInt = basicInt(s.toString())
 
-fun basicInt(b : Byte) : BasicInt = basicInt(b.toLong())
+fun basicInt(b : Byte) : BasicInt = basicInt(b.toString())
 
-fun basicInt(l : Long) : BasicInt {
-    var n = l
-    var arr: IntArray = IntArray(0)
-    var sgn: Boolean = n < 0
-    if(sgn) n = -n
-    var index : Int = 0
-    while(n > maxSimpleInt){
-        val dig : Int = (n % basicDivisor).toInt()
-        arr[index] = dig
-        n = n / basicDivisor
-        index++
-    }
-    arr[index] = n.toInt()
+fun basicInt(l : Long) : BasicInt = basicInt(l.toString())
 
-    return object : BasicInt {
-        override val number : IntArray = arr
-        override val sign : Boolean = sgn
-    }
+fun basicInt(s : String) : BasicInt = object : BasicInt {
+    override val number : BigInteger = BigInteger(s)
 }
 
-fun basicInt(s : String) : BasicInt {
-    // TODO: control w/ regex if s only has numbers
-    return basicInt(s.toLong())
-}
-
-fun basicInt(num : IntArray, sgn: Boolean = false) : BasicInt {
+fun basicInt(num : BigInteger, sgn: Boolean = false) : BasicInt {
     // TODO: check if num has desired format
     return object : BasicInt {
         override val number = num
--- traits/src/net/pureal/traits/math/BasicReal.kt
+++ traits/src/net/pureal/traits/math/BasicReal.kt
@@ -1,36 +1,39 @@
 package net.pureal.traits.math
 
+import java.math.BigInteger
+import java.lang.Math.*
+import kotlin.math.*
+
+
 public trait BasicReal : Number, Comparable<BasicReal> {
-    override fun toDouble() : Double {
-        return 0.0
-    }
+    override fun toDouble() : Double = number.doubleValue() * exponentialFactor()
 
-    override fun toFloat() : Float = toDouble().toFloat()
+    override fun toFloat() : Float = number.floatValue() * exponentialFactor().toFloat()
 
-    override fun toLong() : Long {
-        return 0
-    }
+    override fun toLong() : Long = number.longValue() * exponentialFactor().toLong()
 
     override fun toInt() : Int = toLong().toInt()
     override fun toShort() : Short = toLong().toShort()
     override fun toByte() : Byte = toLong().toByte()
     override fun toChar() : Char = toLong().toChar()
 
+    fun toBigInteger() : BigInteger = number * BigInteger(exponentialFactor().toString())
+
     override fun compareTo(other : BasicReal) : Int {
-        if(other is BasicReal) return other.compareTo(this)
+        if(this.sign == 0) return -other.sign
         if(this.sign != other.sign){
-            return when {
-                this.equals(0) -> 0
-                this.sign -> 1
-                else -> -1
-            }
+            return this.sign-other.sign
         }
-        return 0
+        if(compareExponentTo(other) != 0L) return compareExponentTo(other).toInt() * this.sign
+        return (this-other).number.compareTo(BigInteger.ZERO) * this.sign
     }
 
-    fun compareExponentTo(other: BasicReal) : Long {
-        return this.highestPossibleExponent() - other.highestPossibleExponent()
+    fun compareExponentTo(other : BasicReal) : Long {
+        return this.exponent - other.exponent
     }
+
+    fun exponentialFactor() : Double = pow(10.0,exponent.toDouble())
+
 
     fun compareTo(other : Number) : Int {
         when (other) {
@@ -108,72 +111,81 @@ public trait BasicReal : Number, Comparable<BasicReal> {
     /**
      * This is real magic:
      *
-     * Explanation: number stores large int as array of 32 bit Integers
-     * Each stands for 9 decimal digits (values 0 to 999999999 aka maxSimpleInt)
-     * This makes 1. conversions to/from String easy and
-     *            2. there will no problems with Integer signs
-     *            (even 2 * maxSimpleInt is still positive)
-     * to be exact: only 2^31 * 9 digits are possible, should be enough for any purpose
-     *
-     * the 10-exponent is Long as we do not expect exponents near 2^63
+     * number stores a big Integer ...
+     * and exponent is the 10-exponent - is Long as we do not expect exponents near 2^63
      */
     val exponent : Long
-    val number : IntArray
+    val number : BigInteger
 
-    val sign : Boolean
+    val sign : Int get() = number.signum()
 
-    fun minus() : BasicReal = basicReal(number, exponent, !sign)
+
+    fun minus() : BasicReal = basicReal(-number, exponent)
 
     override fun equals(other : Any?) : Boolean
     {
         when (other) {
             null -> return false
             is Number -> return compareTo(other)==0
+            is BigInteger -> return other==toBigInteger()
             else -> return false
         }
     }
 
-    fun highestPossibleExponent() : Long {
-        return exponent
-    }
-
-    fun highestSignificantDigit() : Long {
-        return exponent + number.size * 9
-    }
-
     fun isInteger() : Boolean = exponent >= 0
 
-    // TODO : Evaluate if highestSignificantDigit & Co. should be calculated at object generation as they are needed for calculations
+    fun signum() : Int = number.signum()
 
+    fun setToExponent(exp : Long) : BasicReal {
+        if (exp == exponent) return this
+        return basicReal(number / BigInteger(pow(10.0,(exp-exponent).toDouble()).toString()))
+    }
 }
 
 
 
-fun basicReal(s : String) : BasicReal = basicReal(0) // TODO: THE important generation function
+fun basicReal(s : String) : BasicReal {
+    var str : String = s.capitalize()
+    var estr : String
+    // with regex - remove illegal characters and whitespace
+    str = str.replaceAll("[^0-9\\.\\-\\+E]","")
 
-fun basicReal(num : IntArray, exp : Long = 0, sgn : Boolean = false) : BasicReal = object : BasicReal {
-    override val number : IntArray = num
-    override val sign : Boolean = sgn
+
+
+    // look if we have exponent
+    var len : Int = str.length
+    var epos : Int = str.indexOf("E")
+    var exp : Long = 0
+    if(epos != -1){
+        estr = str.substring(epos+1)
+        exp = estr.toLong()
+        str = str.substring(0,epos)
+    }
+
+
+    // now remove the "dot of the number"
+
+    val num : BigInteger = BigInteger(str)
+
+
+}
+
+fun basicReal(num : BigInteger, exp : Long = 0) : BasicReal = object : BasicReal {
+    override val number : BigInteger = num
     override val exponent : Long = exp
 }
 
 fun basicReal(bi : BasicInt, exp : Long) : BasicReal = object : BasicReal {
-    override val number : IntArray = bi.number
-    override val sign : Boolean = bi.sign
+    override val number : BigInteger = bi.number
     override val exponent : Long = exp
 }
 
+
+
 fun basicReal(d : Double) : BasicReal = basicReal(d.toString())
-
 fun basicReal(f : Float) : BasicReal = basicReal(f.toString())
+fun basicReal(l : Long) : BasicReal = basicReal(l.toString())
+fun basicReal(i : Int) : BasicReal = basicReal(i.toString())
+fun basicReal(s : Short) : BasicReal = basicInt(s.toString())
+fun basicReal(b : Byte) : BasicReal = basicInt(b.toString())
 
-fun basicReal(l : Long) : BasicReal = basicInt(l)
-
-fun basicReal(i : Int) : BasicReal = basicInt(i)
-
-fun basicReal(s : Short) : BasicReal = basicInt(s)
-
-fun basicReal(b : Byte) : BasicReal = basicInt(b)
-
-val maxSimpleInt =  999999999.toInt()
-val basicDivisor = 1000000000.toInt()
