b7772611cb31f4cc9b5eb017b2c6fa90c9b8c4c3
Maximilian Wende <maximilian.wende@googlemail.com>
2014-07-05 13:58:03 +0200

    removed references to Number due to compiler bug, changed functions slightly

--- tests.traits/src/net/pureal/tests/traits/BasicRealSpecs.kt
+++ tests.traits/src/net/pureal/tests/traits/BasicRealSpecs.kt
@@ -2,15 +2,16 @@ package net.pureal.tests.traits.math
 
 import org.spek.*
 import net.pureal.traits.math.*
+import java.math.BigInteger
 
 public class BasicRealSpecs : Spek() {{
     given("the String '212'"){
         val str : String = "212"
         on("creating a basic Real of it") {
-            val br : BasicReal = basicReal(str)
+            val br : BasicReal = BasicReal(str)
             it("should be 212E+0") {
                 shouldEqual(0L,br.exponent)
-                shouldEqual(BigInteger(212),br.number)
+                shouldEqual(BigInteger(212.toString()),br.number)
             }
         }
     }
