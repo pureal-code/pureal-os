89ee92a1ffaa79f94102b0f79a5f665321dd6078
Julius Kunze <juliuskunze@gmail.com>
2014-07-29 18:52:30 +0200

    Font

--- tests.traits/src/net/pureal/tests/traits/ComposedSpecs.kt
+++ tests.traits/src/net/pureal/tests/traits/ComposedSpecs.kt
@@ -6,19 +6,38 @@ import net.pureal.traits.graphics.*
 import net.pureal.traits.math.*
 
 class ComposedSpecs : Spek() {{
-    given("a composed element") {
-        val elements = listOf<Element<Unit>>(
-                coloredElement(content = Unit.VALUE, shape = circle(0.6), fill = Fills.solid(Colors.red)),
-                coloredElement(content = Unit.VALUE, shape = rectangle(vector(1,1)), fill = Fills.solid(Colors.blue)))
+    given("a composed element of a rectangle and a circle") {
+        val c = coloredElement(content = Unit.VALUE, shape = circle(0.6), fill = Fills.solid(Colors.red))
+        val r = coloredElement(content = Unit.VALUE, shape = rectangle(vector(1, 1)), fill = Fills.solid(Colors.blue))
+        val elements = listOf<Element<Unit>>(c, r)
+
         val x = composed(observableIterable(elements map {transformedElement(it)}))
 
         on("getting the shape") {
             val s = x.shape
 
-            it("should be the concetenated shape") {
+            it("should be the concatenated shape") {
                 shouldBeTrue(s.contains(vector(0, 0.59)))
                 shouldBeTrue(s.contains(vector(0.499, 0.499)))
                 shouldBeFalse(s.contains(vector(0.51, 0.35)))
+            }
+        }
+
+        on("getting the elements at a location contained by all elements") {
+            val e = x.elementsAt(vector(0,0))
+
+            it("should return all elements") {
+                shouldEqual(2, e.count())
+                shouldEqual(c, e.elementAt(0))
+                shouldEqual(c, e.elementAt(1))
+            }
+        }
+
+        on("getting the elements at a location contained only by the circle") {
+            val e = x.elementsAt(vector(0, 0.59))
+
+            it("should return the circle") {
+                shouldEqual(c, e.single())
             }
         }
     }
--- tests.traits/src/net/pureal/tests/traits/FixedTextSpecs.kt
+++ tests.traits/src/net/pureal/tests/traits/FixedTextSpecs.kt
@@ -5,11 +5,22 @@ import net.pureal.traits.*
 import net.pureal.tests.traits.*
 import net.pureal.traits.math.rectangle
 import net.pureal.traits.graphics.fixedText
+import net.pureal.traits.graphics.Font
+import net.pureal.traits.math.BoundedShape
+import net.pureal.traits.graphics.Fill
+import net.pureal.traits.graphics.Fills
+import net.pureal.traits.graphics.Colors
 
 class FixedTextSpecs : Spek() {{
     given("a fixed text element") {
         val s = "Kotlin rocks on the rocks!"
-        val x = fixedText(content = s, size = vector(0.05, 0.01))
+        val x = fixedText(
+                content = s,
+                font = object : Font {
+                    override val size = 0.01
+                    override fun shape(text: String) = rectangle(vector(text.length(), 1))
+                },
+                fill = Fills.solid(Colors.black))
 
         on("getting the content") {
             val c = x.content
